5290da62cfa534ccfcbb77a0c15edb31bd3b8407
LojainHamza <lojainehab17@gmail.com>
2023-07-18 17:16:58 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,8 @@
-#ifndef main_h
-#define main_h
-
-int _putchar(char c);
+int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _is lower(int C);
-int _isalpha(int C);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -14,5 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
